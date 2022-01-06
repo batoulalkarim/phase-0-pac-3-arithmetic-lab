da0955ec3108112ec1fef1143532b7491f03790f
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-01-06 13:37:56 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+  return a + b;
+}
+console.log(add(1000, 81));
+
+function subtract(b, a){
+    return b - a;
+}
+console.log(subtract(220, 1));
+function multiply(a, b){
+    return a * b;
+}
+console.log(multiply(121095, 2));
+
+function divide(a, b) {
+  return a / b;
+}
+console.log(divide(23.8235294118, 2));
+
+function increment(n){
+    return n+1;
+}
+console.log(increment(3));
+
+function decrement(n){
+    return n-1;
+}
+console.log(decrement(4));
+
+
+function makeInt(num){
+    parseInt(num, 10);
+    return parseInt(num, 10);
+}
+function preserveDecimal(n){
+    parseFloat("80.123999")
+    return parseFloat(n);
+}
